cbe2810bf50a6d1598ca027c6803136f12f800eb
JangHun <ytre4386@naver.com>
2017-05-08 16:22:06 +0900

    logout 로직 추가

--- football/src/main/java/com/app/football/user/web/LoginController.java
+++ football/src/main/java/com/app/football/user/web/LoginController.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,6 +69,13 @@ public class LoginController {
 		loginService.uptUserLoginFailcnt(id);
 		UserVo userVo = loginService.getUserByloginfail(id);
 		return userVo;	  
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpSession session)
+	{
+		session.invalidate();
+		return "redirect:/";
 	}
 	
 	
