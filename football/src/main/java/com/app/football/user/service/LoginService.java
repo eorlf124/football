59e91bf2a47b4e9032fcd8689ae3f039a336ba88
core1 <core1@core1-PC>
2017-05-08 14:28:02 +0900

    머지

--- football/src/main/java/com/app/football/user/service/LoginService.java
+++ football/src/main/java/com/app/football/user/service/LoginService.java
@@ -1,5 +1,16 @@
 package com.app.football.user.service;
 
+import com.app.football.user.model.UserVo;
+
+
 public interface LoginService {
+	
+	/**
+	 * user정보를 조회한다.
+	 * @param vo - 조회할 정보가 담긴 UserVo
+	 * @return 조회한 글
+	 * @exception Exception
+	 */
+	UserVo selectUser(UserVo vo) throws Exception;
 
 }
